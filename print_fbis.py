fb8f20abb73b32ad7e05cc186e8635079f508048
xiaotaohe <taochao1997@qq.com>
2019-10-29 19:11:21 +0800

    打印斐波那契数列

--- print_fbis.py
+++ print_fbis.py
@@ -11,8 +11,20 @@ def fbis(num):
     for i in range(num-2):
         result.append(result[-2]+result[-1])
     return result
-
+def print_fbis(num):
+    a = 0; b = 1;
+    print(a)
+    print(b)
+    i = 2;
+    while(i<num):
+        i += 1
+        tmp = a+b
+        a = b
+        b = tmp
+        print(tmp)
+    return
 def main():
+    print_fbis(10)
     result = fbis(10)
     fobj = open('C:\Python_txt\\fbis.txt','w+')
     for i,num in enumerate(result):
