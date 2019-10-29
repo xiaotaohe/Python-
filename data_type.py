f2840115e24de7babd2e3eb80502ef2c3fab44a0
xiaotaohe <taochao1997@qq.com>
2019-10-29 18:59:57 +0800

    测试数据类型

---
+++ data_type.py
@@ -0,0 +1,27 @@
+# coding=utf8
+
+
+
+a = 3+5.6 #结果为8.6
+print("a = ",a)
+
+b = 2 ** 3 #���Ϊ 8 2^3
+print("b = ",b)
+
+c = 5/2   #���Ϊ2
+print("c = ",c)
+
+d = 5.0/2 #���wei 2.5
+print("d = ",d)
+
+e = 5.0//2 #���Ϊ 2
+print("e = ",e)
+
+f = 10 % 2 #���Ϊ0
+print("f = ",f)
+
+g = not True #���ΪFalse
+print("g = ",g)
+
+h = a == b
+print("h = ",h) #���ΪFalse
